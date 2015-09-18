76cacdf22f644a710515e0b04309c082c9145cc2
vdelavaud <virginie.delavaud@cornis.fr>
2015-09-18 14:33:21 +0200

    ajout page home

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | Performances de Recalage automatique"
+  end
+
+end
